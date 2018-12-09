0ec79c76efe79c6066ac4bb9661022ba64822616
mac <m7798432@163.com>
2018-12-09 22:31:51 +0800

    zookeeper java api使用

---
+++ src/main/java/com/mac/zookeeper/zkclient/SessionDemo.java
@@ -0,0 +1,15 @@
+package com.mac.zookeeper.zkclient;
+
+import org.I0Itec.zkclient.ZkClient;
+
+public class SessionDemo {
+
+    private final static String CONNECTSTRING="192.168.11.129:2181,192.168.11.134:2181," +
+            "192.168.11.135:2181,192.168.11.136:2181";
+
+    public static void main(String[] args) {
+        ZkClient zkClient=new ZkClient(CONNECTSTRING,4000);
+
+        System.out.println(zkClient+" - > success");
+    }
+}
